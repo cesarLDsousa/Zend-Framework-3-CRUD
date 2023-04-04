f5ec8eff3d9dcb62efa94adc37969d08a3ac45d5
Cesar Sousa <cesar.luiz@essentia.com.br>
2023-04-04 11:24:37 -0300

    delete e edit adicionados

--- module/Blog/src/Controller/IndexController.php
+++ module/Blog/src/Controller/IndexController.php
@@ -28,14 +28,14 @@ class IndexController extends AbstractActionController
     public function addAction()
     {
         $form = new PostForm();
-        $form->get('submit')->setValue('Add Post');
+        $form->get('submit')->setValue('CONFIRMAR');
 
         $request = $this->getRequest();
         //se n for post retorna a view normal
         if (!$request->isPost()) {
-            return new ViewModel([
+            return [
                 'form' => $form
-            ]);
+            ];
         }
 
         $post = new Post();
@@ -46,8 +46,63 @@ class IndexController extends AbstractActionController
         }
 
         $post->exchangeArray($form->getData());
+
         $this->table->save($post);
 
         return $this->redirect()->toRoute('post');
     }
+
+    public function editAction()
+    {
+        $id = (Int) $this->params()->fromRoute('id', 0);
+
+        if ($id === 0 or !$id) {
+            return $this->redirect()->toRoute('post');
+        }
+
+        try {
+            $post = $this->table->find($id);
+        } catch(\Exception  $e) {
+            return $this->redirect()->toRoute('post');
+        }
+
+        $form = new PostForm();
+        $form->bind($post); //passando model para o form (preenchendo os campos)
+        $form->get('submit')->setAttribute('value', 'CONFIRMAR');
+
+        $request = $this->getRequest();
+
+        if (!$request->isPost()) {
+            return new ViewModel([
+                'form' => $form,
+                'id' => $id
+            ]);
+        }
+
+        $form->setData($request->getPost());
+
+        if (!$form->isValid()) {
+            return new ViewModel([
+                'form' => $form,
+                'id' => $id
+            ]);
+        }
+
+        $this->table->save($post);
+
+        return $this->redirect()->toRoute('post');
+    }
+
+    public function deleteAction()
+    {
+        $id = (Int) $this->params()->fromRoute('id', 0);
+
+        if ($id === 0 or !$id) {
+            return $this->redirect()->toRoute('post');
+        }
+
+        $this->table->delete($id);
+        return $this->redirect()->toRoute('post');
+    }
+    
 }
--- module/Blog/src/Model/PostTable.php
+++ module/Blog/src/Model/PostTable.php
@@ -19,17 +19,48 @@ class PostTable
         return $resultSet; 
     }
 
-    public function save(Post $post) 
+    public function save($post) 
     {
         $data = [
             'title' => $post->title,
             'content' => $post->content
         ];
 
-        if ((int)$post->id === 0) {
+        $id = (int) $post->id;
+
+        if ($id === 0) {
             $this->tableGateway->insert($data);
             return;
         }
+
+        if (!$this->find($id)) {
+            throw new \RuntimeException(sprintf(
+                'Could not retrieve the row %d', $id
+            ));
+        }
+
+        $this->tableGateway->update($data, ['id' => $id]);
     }
 
+    public function find($id) // return one register
+    {
+        $id = (int) $id;
+        $rowset = $this->tableGateway->select(['id' => $id]);
+        $row = $rowset->current();
+
+        if (!$row) {
+            throw new \RuntimeException(sprintf(
+                'Could not retrieve the row %d', $id
+            ));
+        }
+
+        return $row;
+    }
+
+    public function delete($id){
+        $id = (int) $id;
+        $this->tableGateway->delete([
+            'id' => (int)$id
+        ]);
+    }
 }
